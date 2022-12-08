9d72f94244a176dc6dcba29401418774edf130c3
MikeD88 <mustangmichaeldavis@gmail.com>
2022-12-08 15:29:50 -0600

    backend rebase

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Update with your config settings.
 
 /**
@@ -47,48 +46,3 @@ module.exports = {
     },
   },
 };
-=======
-// Update with your config settings.
-
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
-module.exports = {
-  development: {
-    client: "pg",
-    connection: "postgres://USER_NAME:USER_PASSWORD@localhost/DATABASE_NAME",
-  },
-
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
-  },
-
-  production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
-  },
-};
->>>>>>> AddMember, AddCert, Home Initial
