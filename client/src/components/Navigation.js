b400cb8f4e93ebe14a1be73d2d7f17e309c274c3
theFluidDruid97 <artisan.graph@gmail.com>
2022-12-09 14:08:15 -0700

    remove navigation

--- client/src/components/Navigation.js
+++
@@ -1,6 +0,0 @@
-import React, { useContext } from "react";
-    </>
-              src="https://user-images.githubusercontent.com/111238515/206565037-abbe8c97-1c25-407c-9bab-0744b9551a7f.png"
-              width="90"
-              height="90"
-    <>
